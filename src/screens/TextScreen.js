95e410f25776bcbdf0e382b801f301b950d340af
jose.salazar <jose.salazar@andonix.com>
2020-03-20 19:22:16 -0600

    jose test

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,21 +1,21 @@
-import React, {useState} from 'react';
-import {Text, View, StyleSheet, TextInput} from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
     const [name, setName] = useState('');
 
-    return(
+    return (
         <View>
             <Text>Enter Name:</Text>
-            <TextInput 
-                style={styles.textInput} 
+            <TextInput
+                style={styles.textInput}
                 autoCapitalize="none"
                 autoCorrect={false}
                 value={name}
-                onChangeText={value => setName(value)}                
+                onChangeText={value => setName(value)}
             />
             <Text>Your name is {name}</Text>
-            { name.length < 5 ? <Text>Menor a 5</Text> : null }
+            {name.length < 5 ? <Text>Menor a 5</Text> : null}
         </View>
     );
 };
